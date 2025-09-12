9d7a7530e4c16bc5faf57306988dbcc7938db2ee
Volodymyr Fedchenko <emirwtf@gmail.com>
2025-09-12 21:27:40 +0300

    fixed all errors in hw2

--- test/specs/home_work_2.e2e.ts
+++ test/specs/home_work_2.e2e.ts
@@ -1,37 +1,54 @@
-import { expect } from '@wdio/globals'
+import { expect } from '@wdio/globals';
 
 // зайти на сторінку
 // натиснути на api
 // проскролити до футора
-// перевірити віконце isVisible, isClickible
-// getHTML
+// перевірити віконце isVisible
+// перевірити віконце isClickible
 // click по ньому
 // waitUntil дочекаемось появи заголовку webdriver protokol
 
 describe('home work 2', () => {
        
-    it('check title', async () => {
-        await browser.url('https://webdriver.io/');
-          const title  = await browser.getTitle();  
-           await expect(browser).toHaveTitle( "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO")   
+    it('go to the page webdriver.io', async () => {
+        
+       await browser.url('https://webdriver.io/');       
+       await expect(browser).toHaveTitle( "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO")   
     });
+    
     it('tap api link', async () => {        
            const APIlink = $('[href="/docs/api"]');
+            
             await APIlink.click();
-            await browser.pause(2000) ;      
+            await browser.pause(2000);      
     });
+    
     it('scroll to footer', async () => {        
-           const window = $('.pagination-nav__link');
-            await window.scrollIntoView();
-            await browser.pause(2000) ;      
-            await window.isDisplayed();
-            await window.isClickable();
-            await window.click();
+           
+       const button = $('.pagination-nav__link');            
+         await button.scrollIntoView();           
     });
-    it('check if h1 displayed', async () => {        
-           const window = $('.pagination-nav__link');           
-           await window.click();
-           const title = $('H1');
-           await title.isDisplayed();
+    
+    it('check if button visible', async () => {        
+           const button = $('.pagination-nav__link');                
+            await expect(button).toBeDisplayed();
+            await browser.pause(2000);            
+    });
+    
+    it('check if button clicable', async () => {        
+           const button = $('.pagination-nav__link');            
+             const clicable = await button.isClickable();
+              await expect(clicable).toBe(true);       
+    });
+    
+    it('click on the button', async () => {        
+           const button = $('.pagination-nav__link');           
+            await button.click();
+    });
+    
+    it('check if h1 displayed', async () => {           
+         const header = $('H1');
+           await header.waitForDisplayed({timeout:3000});
+           await expect(header).toBeDisplayed();
     });    
 });
