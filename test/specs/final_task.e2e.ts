16e62371159c280da0a98db5c1ea0ca3d67ec8a3
Volodymyr Fedchenko <emirwtf@gmail.com>
2025-09-15 16:36:58 +0300

    comment deleted

--- test/specs/final_task.e2e.ts
+++ test/specs/final_task.e2e.ts
@@ -1,44 +1,5 @@
 import { expect } from '@wdio/globals';
 
-//git hub
-//sign up
-//перевіряемо на відображення текст реєстрації
-//email continiue
-//password continiue
-//ркєстрація до кінця це перший тест кейс
-
-//second git hub scloll till tekst
-//hceck if is header visible
-//statrt free triasl linl cechk if it visible
-//and click
-//pick you trial plan check if it visible
-//enterprise cloud click
-
-//therd git hub
-//scroll down
-//button subscribe in footer isclicable
-//click
-//../newsletter url
-//h1 is visible
-//enter email
-//chose contry
-//clis=ck chackbox
-//subscribe click
-//thanks for subscribe chick if it visble
-
-//fourth github
-//input search tot jamp
-//text act 
-//press theline below
-//перевірити що те шо ми ввели е у списку відображеному по href
-
-//fifth git hub
-//pricing click
-//get the complit..... is visible
-//scroll to compare all features
-//click
-//compare features is visiblу
-
 describe('sign up on github and check if search work', () => {
 
     beforeEach(async () => {
